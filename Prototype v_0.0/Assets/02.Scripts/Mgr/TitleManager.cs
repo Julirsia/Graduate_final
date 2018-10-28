8e20dbe2c65daa0c9343f24ad9f8c87671968c17
JOSH\rml41 <rml416@naver.com>
2018-10-28 22:25:00 +0900

    타이틀 화면 버튼액션 일부 추가

---
+++ Prototype v_0.0/Assets/02.Scripts/Mgr/TitleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject Title;
+    public GameObject ModeSelection;
+    public GameObject ComingSoonPanel;
+
+    public void ButtonActionQuit()
+    {
+        Application.Quit();    
+    }
+
+    public void ButtonActionModeChange()
+    {
+        Title.SetActive(false);
+        ModeSelection.SetActive(true);
+    }
+
+    public void ButtonActionComingSoon()
+    {
+        ComingSoonPanel.SetActive(true);
+        StartCoroutine(ComingSooPanelDisappear());
+    }
+    IEnumerator ComingSooPanelDisappear()
+    {
+        yield return new WaitForSeconds(4f);
+        ComingSoonPanel.SetActive(false);
+    }
+}
